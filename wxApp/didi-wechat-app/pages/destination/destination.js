0e9be11b46a12cceb60dcdf6b6b60147b8143b0e
zukangChen <1054112145@qq.com>
2019-05-06 10:14:34 +0800

    滴滴一夏

---
+++ wxApp/didi-wechat-app/pages/destination/destination.js
@@ -0,0 +1,104 @@
+// pages/destination/destination.js
+const app = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  searchInput(e){
+    const value = e.detail.value;
+    app.globalData.qqmapsdk.getSuggestion({
+      keyword: value,
+      region : '南昌',
+      success: (res) =>{
+        this.setData({
+          address: res.data
+        })
+      }
+    })
+  },
+  toIndex(e){
+    //地址简写
+    const destination = e.currentTarget.dataset.destination;
+    //详细地址
+    const endAddress = e.currentTarget.dataset.end;
+    app.globalData.destination = destination;
+    //得到经纬度
+    app.globalData.qqmapsdk.geocoder({
+      address : endAddress,
+      success: (res) => {
+        console.log('得到经纬度', res);
+        app.globalData.endLatitude = res.result.location.lat;
+        app.globalData.endLongitude = res.result.location.lng;
+        wx.navigateTo({
+          url: '../index/index',
+        })
+      },
+      fail : () =>{
+        wx.showToast({
+          title : '选择的地址中必须包含城市名称'
+        })
+      }
+    })
+    
+  }
+})
