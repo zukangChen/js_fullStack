9b034e46ef08759765351d5e5fc795763e07cde1
zukangChen <1054112145@qq.com>
2019-06-04 21:48:36 +0800

    vue router

---
+++ vue-learn/vue-router/router/src/components/LookTwo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="look">
+    {{message}}
+    <p>接受的参数:{{$route.params.id}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Look',
+  data () {
+    return {
+      message: '我'
+    }
+  }
+}
+</script>
